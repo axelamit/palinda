df11d84a0aaad1ed33bf48e75cf4ac47391e8a61
axelamit <axel.v.nilsson@gmail.com>
2021-03-16 17:32:18 +0100

    add two tests

--- rmarte-palinda-1-master/rmarte-palinda-1-master/src/twopartsum/twopartsum_test.go
+++ rmarte-palinda-1-master/rmarte-palinda-1-master/src/twopartsum/twopartsum_test.go
@@ -16,4 +16,28 @@ func TestSumConcurrentCorrectlySumsEvenArray(t *testing.T) {
 	}
 }
 
+// odd length
+func TestSumConcurrentlySumsOddArray(t *testing.T) {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+	expected := 66
+
+	actual := ConcurrentSum(arr)
+
+	if actual != expected {
+		t.Errorf("expected %d, was %d", expected, actual)
+	}
+}
+
+// negative numbers
+func TestSumConcurrentlySumsNegativeNumbers(t *testing.T) {
+	arr := []int{-1, 1, -2, 5, 4, -4, -5}
+	expected := -2
+
+	actual := ConcurrentSum(arr)
+
+	if actual != expected {
+		t.Errorf("expected %d, was %d", expected, actual)
+	}
+}
+
 // TODO add at least two more test cases!
